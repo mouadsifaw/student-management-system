429ad25c8a4b91464aa23785b18c61059c408f90
Mouad Ait Khouya <104041002+mouadsifaw@users.noreply.github.com>
2025-04-01 18:33:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash  # Add flash here
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
